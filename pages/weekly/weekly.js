1e2465b93e969431d2bf116728b879c5029219fa
Serendipityy96 <justserendipity96@gmail.com>
2018-07-01 11:09:01 +0800

    改成列表布局，wx:for,wxx:if,模板字符串

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -1,1 +1,25 @@
-Page({})
+Page({
+  data: {
+    weeklyMovieList:[{
+      name: "侏罗纪世界1",
+      comment: "hhh",
+      imagePath: "/images/zljsj.jpg",
+      isHighlyRecommended:true
+    },
+      {
+        name: "侏罗纪世界2",
+        comment: "mmm",
+        imagePath: "/images/zljsj.jpg",
+        isHighlyRecommended: true
+      },
+      {
+        name: "侏罗纪世界3",
+        comment: "lll",
+        imagePath: "/images/zljsj.jpg",
+        isHighlyRecommended: true
+      }
+    ],
+    count: 123,
+    score: 95
+  }
+})
