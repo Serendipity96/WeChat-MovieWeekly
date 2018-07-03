d854022589835911d7b2217c6036ba8a1ab24bb4
Serendipityy96 <justserendipity96@gmail.com>
2018-07-03 20:03:46 +0800

    学习onload事件，初探传参机制

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -1,11 +1,11 @@
 Page({
   data: {
-    weeklyMovieList:[{
-      name: "侏罗纪世界1",
-      comment: "hhh",
-      imagePath: "/images/zljsj.jpg",
-      isHighlyRecommended:true
-    },
+    weeklyMovieList: [{
+        name: "侏罗纪世界1",
+        comment: "hhh",
+        imagePath: "/images/zljsj.jpg",
+        isHighlyRecommended: true
+      },
       {
         name: "侏罗纪世界2",
         comment: "mmm",
@@ -21,5 +21,16 @@ Page({
     ],
     count: 123,
     score: 95
+  },
+  onLoad: function() {
+    this.setData({
+      currentIndex: this.data.weeklyMovieList.length - 1 //如果data中没有currentIndex，那就创建一个并赋值
+    })
+  },
+  f0: function() {
+    this.setData({
+      count: this.data.count + 1,
+      "weeklyMovieList[2].name" :"改名字"
+    })
   }
 })
